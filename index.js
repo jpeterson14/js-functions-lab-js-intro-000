a480e2b1416fa660669d4204b3924902b4d03cd9
Jake Peterson <jpeterson0514@gmail.com>
2021-04-09 23:45:43 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,10 @@
+// write your code below!
+function happyHolidays(){
+  return "Happy holidays!"
+}
+function happyHolidaysTo(name){
+  return `Happy holidays, ${name}!`
+}
+function happyCustomHolidayTo(days,holiday){
+  return `It's ${days} days until ${holiday}!`
+}
